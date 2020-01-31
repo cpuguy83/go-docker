d09142c6b453216b90cabf960c4b187aebd3d31c
Brian Goff <cpuguy83@gmail.com>
2020-01-31 10:19:03 -0800

    add test for container remove

---
+++ container/remove_test.go
@@ -0,0 +1,28 @@
+package container
+
+import (
+	"context"
+	"testing"
+
+	"github.com/cpuguy83/go-docker/errdefs"
+	"gotest.tools/assert"
+)
+
+func TestRemove(t *testing.T) {
+	ctx := context.Background()
+	s := newTestService(t)
+
+	err := s.Remove(ctx, "notexist")
+	assert.Check(t, errdefs.IsNotFound(err))
+
+	c, err := s.Create(ctx, WithCreateImage("busybox:latest"))
+	assert.NilError(t, err)
+	assert.Check(t, s.Remove(ctx, c.ID()), "leaked container: %s", c.ID())
+
+	c, err = s.Create(ctx, WithCreateImage("busybox:latest"))
+	assert.Assert(t, c.Start(ctx))
+	err = s.Remove(ctx, c.ID())
+	assert.Assert(t, errdefs.IsConflict(err), err)
+	err = s.Remove(ctx, c.ID(), WithRemoveForce)
+	assert.NilError(t, err)
+}
