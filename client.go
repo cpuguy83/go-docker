6e2fd2cecd971d8782617491bfe76d6a3d503aa3
Brian Goff <cpuguy83@gmail.com>
2020-01-31 08:46:43 -0800

    Add some docstrings for the main client

--- client.go
+++ client.go
@@ -4,17 +4,31 @@ import (
 	"github.com/cpuguy83/go-docker/transport"
 )
 
+// Client is the main docker client
+// Create one with `NewClient`
 type Client struct {
 	tr transport.Doer
 }
 
+// NewClientConfig is the list of options for configuring a new docker client
 type NewClientConfig struct {
+	// Sets the API version to use when communicating with Docker
 	APIVersion string
-	Transport  transport.Doer
+	// Transport is the communication method for reaching a docker engine instance.
+	// You can implement your own transport, or use the ones provided in the transport package.
+	// If this is unset, the default transport will be used (unix socket connected to /var/run/docker.sock).
+	Transport transport.Doer
 }
 
 type NewClientOption func(*NewClientConfig)
 
+// NewClient creates a new docker client
+// You can pass in options using functional arguments.
+//
+// If no transport is provided as an option, the default transport will be used.
+//
+// You probably want to set an API version for the client to use here.
+// See `NewClientConfig` for available options
 func NewClient(opts ...NewClientOption) *Client {
 	var cfg NewClientConfig
 	for _, o := range opts {
@@ -30,5 +44,3 @@ func NewClient(opts ...NewClientOption) *Client {
 	}
 	return &Client{tr: tr}
 }
-
-
--- container.go
+++ container.go
@@ -4,6 +4,7 @@ import (
 	"github.com/cpuguy83/go-docker/container"
 )
 
+// ContainerService provides access to container functionaliaty, such as create, delete, start, stop, etc.
 func (c *Client) ContainerService() *container.Service {
 	return container.NewService(c.tr)
 }
