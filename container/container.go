13cc3703b5af73dc975c877e9edc17e01b1c26d5
Brian Goff <cpuguy83@gmail.com>
2020-01-31 12:22:32 -0800

    More docs around new container

--- container/container.go
+++ container/container.go
@@ -6,11 +6,13 @@ import (
 	"github.com/cpuguy83/go-docker/transport"
 )
 
+// Container provides bindings for interacting with a container in Docker
 type Container struct {
 	id string
 	tr transport.Doer
 }
 
+// ID returns the container ID
 func (c *Container) ID() string {
 	return c.id
 }
@@ -22,20 +24,16 @@ type NewConfig struct {
 // NewOption is used as functional parameters to `New`
 type NewOption func(*NewConfig)
 
-// NewContainer instantiates a container object that you can interact with the Service's transport
-// See the unbounded `New` function for more details
-func (s *Service) NewContainer(ctx context.Context, name string, opts ...NewOption) *Container {
-	return New(ctx, s.tr, name, opts...)
-}
-
-// New creates a new container object in memory, it does not interact with the Docker API at all.
+// New creates a new container object in memory. This function does not interact with the Docker API at all.
 // If the container does not exist in Docker, all calls on the Container will fail.
 //
-// To actually create a container you must call `Create` on the container service.
-func New(_ context.Context, tr transport.Doer, name string, opts ...NewOption) *Container {
+// To actually create a container you must call `Create` first (which will return a container object to you).
+//
+// TODO: This may well change to actually inspect the container and fetch the actual container ID.
+func (s *Service) NewContainer(_ context.Context, id string, opts ...NewOption) *Container {
 	var cfg NewConfig
 	for _, o := range opts {
 		o(&cfg)
 	}
-	return &Container{id: name, tr: tr}
+	return &Container{id: id, tr: s.tr}
 }
