a179580cb9dc37d944a309dc46c88e264de6b588
Brian Goff <cpuguy83@gmail.com>
2020-02-06 23:24:18 -0800

    Add container stop test

---
+++ container/stop_test.go
@@ -0,0 +1,34 @@
+package container
+
+import (
+	"context"
+	"testing"
+
+	"github.com/cpuguy83/go-docker/errdefs"
+	"gotest.tools/assert"
+)
+
+func TestStop(t *testing.T) {
+	ctx := context.Background()
+	s := newTestService(t)
+
+	c := s.NewContainer(ctx, "notexist")
+	err := c.Stop(ctx)
+	assert.Assert(t, errdefs.IsNotFound(err), err)
+
+	c, err = s.Create(ctx, WithCreateImage("busybox:latest"),
+		WithCreateCmd("/bin/sh", "-c", "trap 'exit 1' EXIT; while true; do sleep 1; done"),
+	)
+	defer func() {
+		if c != nil {
+			assert.Check(t, s.Remove(ctx, c.ID(), WithRemoveForce))
+		}
+	}()
+	assert.NilError(t, err)
+	assert.NilError(t, c.Start(ctx))
+	assert.NilError(t, c.Stop(ctx))
+
+	inspect, err := c.Inspect(ctx)
+	assert.NilError(t, err)
+	assert.Assert(t, !inspect.State.Running)
+}
