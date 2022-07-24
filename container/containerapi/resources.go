03a411c67c5428f5f551618c7b5817778e37aba1
Brian Goff <cpuguy83@gmail.com>
2022-07-24 16:19:17 +0000

    Remove go-units as a dependency

--- container/containerapi/resources.go
+++ container/containerapi/resources.go
@@ -1,9 +1,5 @@
 package containerapi
 
-import (
-	"github.com/docker/go-units"
-)
-
 // Resources contains container's resources (cgroups config, ulimits...)
 type Resources struct {
 	// Applicable to all platforms
@@ -35,11 +31,18 @@ type Resources struct {
 	MemorySwappiness     *int64          // Tuning container memory swappiness behaviour
 	OomKillDisable       *bool           // Whether to disable OOM Killer or not
 	PidsLimit            *int64          // Setting PIDs limit for a container; Set `0` or `-1` for unlimited, or `null` to not change.
-	Ulimits              []*units.Ulimit // List of ulimits to be set in the container
+	Ulimits              []*Ulimit       // List of ulimits to be set in the container
 
 	// Applicable to Windows
 	CPUCount           int64  `json:"CpuCount"`   // CPU count
 	CPUPercent         int64  `json:"CpuPercent"` // CPU percent
 	IOMaximumIOps      uint64 // Maximum IOps for the container system drive
 	IOMaximumBandwidth uint64 // Maximum IO in bytes per second for the container system drive
+}
+
+// Ulimit represents the string name of an linux rlimit along with the hard/soft limits.
+type Ulimit struct {
+	Name string
+	Hard int64
+	Soft int64
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.13
 
 require (
 	github.com/Microsoft/go-winio v0.4.15
-	github.com/docker/go-units v0.4.0
 	github.com/google/go-cmp v0.4.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	gotest.tools v2.2.0+incompatible
